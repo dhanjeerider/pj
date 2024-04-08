d3028b982e6f3177d14d7a390eff6ace3cb433c2
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-08 13:14:16 +0530

    Create Men.js

---
+++ Men.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+            var correctPassword = "dktechnozone";
+            var enteredPassword = "dkechnozone";  
+            if (!enteredPassword || enteredPassword !== correctPassword) {
+                window.location.href = "https://www.dktechnozone.in";
+alert("your site is using this theme without permission of theme owner you need to purchase its licence key"); }
+        });
