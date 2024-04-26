a8d6b8aef60373a4ca21b7a6a288c738f93046ad
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 11:18:14 +0530

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -1,7 +1,6 @@
-<script>
 if (window.location.href.startsWith("https://dkheok.blogspot.com")) { document.getElementById("main").style.display = "block";
 } else {
 document.getElementById("main").style.display = "none";
 alert("Credit expired.");
 }
-</script>
+
