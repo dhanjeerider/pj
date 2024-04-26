8fd828aa7c86da5f69290ae4c04bfd381505e3b2
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 12:02:34 +0530

    Create Icons.js

---
+++ Icons.js
@@ -0,0 +1,5 @@
+if (window.location.href.startsWith("https://dkheok.blogspot.com")) { document.getElementById("main").style.display = "block";
+} else {
+document.getElementById("main").style.display = "none";
+alert("Credit expired.");
+}
