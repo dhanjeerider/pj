475257cfdcf46db0f4ecd3a94d9a91c73078c868
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 11:17:38 +0530

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -1,5 +1,5 @@
 <script>
-if (window.location.href.startsWith("https://dkhek.blogspot.com")) { document.getElementById("main").style.display = "block";
+if (window.location.href.startsWith("https://dkheok.blogspot.com")) { document.getElementById("main").style.display = "block";
 } else {
 document.getElementById("main").style.display = "none";
 alert("Credit expired.");
