177f1ac006a7f59fdd744543d5ddae12a2fb98fe
GaaCBot <noreply@gaac.vercel.app>
2024-04-26 14:39:44 +0530

    Uploaded auther.js via GAAC

---
+++ uploads/auther.js
@@ -0,0 +1,12 @@
+if (window.location.href.startsWith("https://dkhek.blogspot.com/")) {
+  document.getElementById("main").style.display = "block"; }
+   else {
+alert("Credit expired.");
+document.getElementById("main").style.display = "none";
+}
+if (window.location.href.startsWith("https://124x.blogspot.com/")) {
+  document.getElementById("main").style.display = "block"; }
+   else {
+alert("Credit expired.");
+document.getElementById("main").style.display = "none";
+}
