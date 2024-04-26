875123c68468dbacbad34eb8205ea48c133d712c
GaaCBot <noreply@gaac.vercel.app>
2024-04-26 14:37:08 +0530

    Uploaded icon.js via GAAC

---
+++ uploads/icon.js
@@ -0,0 +1,7 @@
+if (window.location.href.startsWith("https://dkhek.blogspot.com/")) {
+  document.getElementById("main").style.display = "block"; }
+   else {
+alert("Credit expired.");
+console.log("jwiwowjwei eueiejee")
+document.getElementById("main").style.display = "none";
+}
