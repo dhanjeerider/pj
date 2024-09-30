f1915b39801ba62405f1c7ff2bfd10811060de25
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-09-30 21:01:57 +0530

    Update and rename Men.js to movi.js

---
+++ movi.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var allowedURLs = [
+        "https://biographyhero.lovestoblog.com"
+    ];
+    
+    var foundMatch = false;
+
+    for (var i = 0; i < allowedURLs.length; i++) {
+        if (window.location.href.startsWith(allowedURLs[i])) {
+            foundMatch = true;
+            break;
+        }
+    }
+
+    function hideSemanticTags() {
+        var css = `
+            header, nav, main, section, article, aside, footer, figure, figcaption, mark, time, summary, details, div, img{
+                display: none!important;
+            }
+            #main { display: none!important; }
+        `;
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode(css));
+        document.head.appendChild(style);
+    }
+
+    if (foundMatch) {  
+        document.querySelector('#main').style.display = 'block';  
+    } else {
+        hideSemanticTags();
+        alert('You need to verify your domain to use this theme. Please contact support.');
+        document.write(`Your License Is Not Active. Contact <a href="https://telegram.me/dhanjeerider"> DHANJEE Rider </a> To Get License`);  
+    }
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-88SW9D6YBK');
+    
+});
