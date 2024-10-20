165b2f4d5c70e9d4aba26f8da5bf063d6c6d53e4
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-10-20 21:43:54 +0530

    Update movi.js

--- movi.js
+++ movi.js
@@ -1,5 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var allowedURLs = [
+var allowedURLs = [
         "https://biographyhero.lovestoblog.com",
         "https://dkflix.000.pe",
         "https://hdhub4u-wp-theme-dktzn.is-best.net",
@@ -13,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     for (var i = 0; i < allowedURLs.length; i++) {
         if (window.location.href.startsWith(allowedURLs[i])) {
-            foundMatch = true;
+            foundMatch = false;
             break;
         }
     }
@@ -35,12 +34,4 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         hideSemanticTags();
         alert('You need to verify your domain to use this theme. Please contact support.');
-            document.body.innerHTML = 'Your License Is Not Active. Contact <a href="https://telegram.me/dhanjeerider">DHANJEE Rider</a> to get a license';
-            }
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-88SW9D6YBK');
-    
-});
+                       }
